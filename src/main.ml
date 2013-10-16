7d713826bc44c305f559fc44d487314a8b748561
Gonzague <gogaz@beer.(none)>
2013-10-16 21:09:38 +0200

    fichiers

---
+++ src/main.ml
@@ -0,0 +1,51 @@
+
+(* init de SDL *)
+let sdl_init () =
+  begin
+    Sdl.init [`EVERYTHING];
+    Sdlevent.enable_events Sdlevent.all_events_mask;
+  end
+ 
+(* attendre une touche ... *)
+let rec wait_key () =
+  let e = Sdlevent.wait_event () in
+    match e with
+    Sdlevent.KEYDOWN _ -> ()
+      | _ -> wait_key ()
+ 
+(*
+  show img dst
+  affiche la surface img sur la surface de destination dst (normalement l'écran)
+*)
+let show img dst =
+  let d = Sdlvideo.display_format img in
+    Sdlvideo.blit_surface d dst ();
+    Sdlvideo.flip dst
+ 
+(* main *)
+let main () =
+  begin
+    (* Nous voulons 1 argument *)
+    if Array.length (Sys.argv) < 2 then
+      failwith "Il manque le nom du fichier!";
+    (* Initialisation de SDL *)
+    sdl_init ();
+    (* Chargement d'une image *)
+    let img = Sdlloader.load_image Sys.argv.(1) in
+    (* On récupère les dimensions *)
+    let (w,h) = Img.get_dims img in
+    (* On crée la surface d'affichage en doublebuffering *)
+    let display = Sdlvideo.set_video_mode w h [`DOUBLEBUF] in
+        (* on affiche l'image *)
+        show img display;
+        (* on attend une touche *)
+        wait_key ();
+        let newSurface = Sdlvideo.create_RGB_surface_format img [] w h in
+                Img.image2grey img newSurface;
+                show newSurface display;
+                wait_key ();
+        (* on quitte *)
+        exit 0
+  end
+ 
+let _ = main ()
---
+++ src/matrix.ml
@@ -0,0 +1,64 @@
+(* fabrique une matrice de largeur w et de hauteur h 
+ * en applicant la fonction f x y *)
+
+let init w h f =
+  let f2 y x = f x y in
+  let fct x = Array.init w ( f2 x ) in
+  Array.init h fct
+
+(* retourne la largeur d'une matrice *)
+let width m = 
+  if Array.length m = 0 then
+    0
+  else
+    Array.length m.(0)
+
+(* retourne la hauteur d'une matrice (Simple binding de Array.length) *)
+let height m =
+  Array.length m
+
+(* Met l'élément v à l'emplacement x y ) *)
+let set m x y v =
+  m.(y).(x) <- v
+
+let get m x y =
+  m.(y).(x)
+
+let iter f m =
+  let parcours m2 = Array.iter f m2 in
+  Array.iter parcours m
+
+let iteri f m =
+  let f2 y x = f x y in
+  let parcours y m2 = Array.iteri (f2 y) m2 in
+  Array.iteri parcours m
+
+(* Transforme une image noir et blanc en une matrice *)
+let from_img img =
+  let f x y = let (c,_,_) =
+      Sdlvideo.get_pixel_color img x y in
+      c in
+  let (w,h) = ((Sdlvideo.surface_info img).Sdlvideo.w, (Sdlvideo.surface_info img).Sdlvideo.h) in
+  init w h f
+
+(* Transforme une matrice en image noir et blanc *)
+let to_img m =
+  let img = Sdlvideo.create_RGB_surface [] (width m) (height m) 24
+  Int32.zero Int32.zero Int32.zero Int32.zero in
+  let copy x y c =
+    Sdlvideo.put_pixel_color img x y (c,c,c) in
+  iteri copy m;
+  img
+
+
+(* Produit des matrices m1 et m2 *)
+let produit m1 m2 =
+  let w = width m1 in
+  let f x y =
+    let r = ref 0 in
+    for i=0 to w do
+      r := !r + m1.(y).(i) * m2.(i).(x)
+    done;
+    !r
+     in
+    init w w f
